86e2ae15275276bde68b4d0797b5aadcb600316d
HARFS <reza81zarisfi@gmail.com>
2026-05-27 08:04:07 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+{
+  "short_name": "Harfs League",
+  "name": "Harfs League System",
+  "icons": [
+    {
+      "src": "https://raw.githubusercontent.com/rzarisfi/HarfsMusic/league-data/icon-192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "https://raw.githubusercontent.com/rzarisfi/HarfsMusic/league-data/icon-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": ".",
+  "display": "standalone",
+  "theme_color": "#0f172a",
+  "background_color": "#0f172a"
+}
